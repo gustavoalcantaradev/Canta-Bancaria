ac22d8419d857a21ea18c1c9798bf779bffd4068
Gustavo Alcantara de Jesus <107977597+gustavoalcantaradev@users.noreply.github.com>
2023-11-28 12:57:55 -0300

    Finalizando projeto conta

--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -11,7 +11,7 @@ public class ContaController implements ContaRepository {
      *  Collection listaContas contendo Objetos do tipo Conta
      * */
     private ArrayList<Conta> listaContas = new ArrayList<Conta>();
-    int numero = 0;
+	int numero = 0;
     
     /**
      *  Procurar Conta por numero
@@ -75,20 +75,42 @@ public class ContaController implements ContaRepository {
 
     @Override
     public void sacar(int numero, float valor) {
-        
+        var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			
+			if(conta.sacar(valor) == true)
+				System.out.println("\nO Saque na Conta numero: " + numero + " foi efetuado com sucesso!");		
+		
+		}else
+			System.out.println("\nA Conta numero: " + numero + " não foi encontrada!");
         
     }
 
     @Override
     public void depositar(int numero, float valor) {
-        
-        
+        var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			conta.depositar(valor);
+			System.out.println("\nO Depósito na Conta numero: " + numero + " foi efetuado com sucesso!");
+		}else
+			System.out.println("\nA Conta numero: " + numero + " não foi encontrada ou a Conta destino não é uma Conta Corrente!");
     }
 
     @Override
     public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-        
-        
+        var contaOrigem = buscarNaCollection(numeroOrigem);
+		var contaDestino = buscarNaCollection(numeroDestino);
+
+		if (contaOrigem != null && contaDestino != null) {
+							
+				if (contaOrigem.sacar(valor) == true) {
+					contaDestino.depositar(valor);
+					System.out.println("\nA Transferência foi efetuado com sucesso!");
+				}
+		}else
+			System.out.println("\nA Conta de Origem e/ou Destino não foram encontradas!");
     }
 
     /** 
@@ -99,7 +121,9 @@ public class ContaController implements ContaRepository {
 	 * Método para gerar automaticamente o Número da Conta
 	 * */
 	public int gerarNumero() {
+				
 		return ++ numero;
+
 	}
 
     /**
@@ -129,4 +153,3 @@ public class ContaController implements ContaRepository {
 	}
     
 }
-
